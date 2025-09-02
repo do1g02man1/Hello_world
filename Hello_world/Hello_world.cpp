6dd93145befdd3d86313c0fbcf3dca922683e105
KGA <KGA@DESKTOP-AJ0DQ6L>
2025-09-02 14:51:55 +0900

    변수와 연산자

--- Hello_world/Hello_world.cpp
+++ Hello_world/Hello_world.cpp
@@ -19,12 +19,18 @@ Ctrl + Space : 자동 완성 기능 (인텔리센스)
 빌드 : 실행파일 생성하기
 리빌드 : 전체 다시 빌드하기(정리 + 빌드)
 
+변수(Variable)
+ - 변하는 숫자
+ - 컴퓨터에 값
+
 */
+
+
 
 #include <iostream> // 입출력 관련
 #include <stdio.h>
 #include <cstdio> // stdio.h에 네임 스페이스 추가한 래퍼
-// #include "header.h"
+//#include "header.h"
 
 int main() // 엔트리 포인트 (코드가 시작되는 곳)
 {
@@ -45,19 +51,34 @@ int main() // 엔트리 포인트 (코드가 시작되는 곳)
 
      */
 
-     //int number = 0;
-    int age = 0;
+    
+    int age;
 
+    // C++ 에서는 정수형 변수에 소수를 넣어도 오류가 나지 않고 소수점 아래를 버린 후 정수형으로 인식한다.
+    // 그래서 age의 값은 5
+    age = 5.3;
+    
     // scanf_s("%d", &number); // C 입력 방법
 
     //std::cin >> number; // C++ 입력 방법
     //printf("입력한 숫자는 : %d\n", number);
 
-    printf("당신의 나이는 몇살인가요? ");
+    /*printf("당신의 나이는 몇살인가요? ");
     std::cin >> age;
-    printf("당신의 나이는 : %d", age);
+    printf("당신의 나이는 : %d", age);*/
 
-    // 브랜치
+    int number1 = 0;
+    int number2 = 0;
+    int number3;
+    printf("숫자 1을 입력하시오 : ");
+    std::cin >> number1;
+
+    printf("숫자 2을 입력하시오 : ");
+    std::cin >> number2;
+
+    number3 = number1 + number2;
+
+    printf("두 숫자의 합은 %d입니다.", number3);
 
     return 0;
 }
